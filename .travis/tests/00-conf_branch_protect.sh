3ba135b139316167666c189e0e9999a420b263b2
Tino Vazquez <tinova@opennebula.org>
2019-04-30 14:52:22 +0200

    we protec

--- .travis/tests/00-conf_branch_protect.sh
+++ .travis/tests/00-conf_branch_protect.sh
@@ -1,21 +1,15 @@
 #!/bin/bash -xv
 
-echo $TRAVIS_BRANCH
-
-# In a PR we compare with the head of $TRAVIS_BRANCH
-# Otherwise, we want to check with the previous commit
-if [[ $TRAVIS_PULL_REQUEST != false ]]; then
-    export HEAD_TO_CHECKOUT="$HEAD"
-else
-    export HEAD_TO_CHECKOUT="$HEAD~1"
-fi
-
-
 if [[ $TRAVIS_BRANCH =~ (^one-) ]]; then
     export PREVIOUS_ONE=~/previous.one
     export CURRENT_ONE=../../
     git clone https://github.com/tinova/one $PREVIOUS_ONE
-    (cd $PREVIOUS_ONE ; git checkout $TRAVIS_BRANCH ; git checkout $HEAD_TO_CHECKOUT)
+    (cd $PREVIOUS_ONE ; git checkout $TRAVIS_BRANCH)
+    # In a PR we compare with the head of $TRAVIS_BRANCH
+    # Otherwise, we want to check with the previous commit
+    if [[ $TRAVIS_PULL_REQUEST == false ]]; then
+        (cd $PREVIOUS_ONE ; git checkout $HEAD~1)
+    fi
     diff $PREVIOUS_ONE/share/etc/oned.conf $CURRENT_ONE/share/etc/oned.conf
 fi
 
