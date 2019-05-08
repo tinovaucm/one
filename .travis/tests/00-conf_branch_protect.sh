08b1b4cbb0a7a64c74397426fd800bb0d0da1b3d
Tino Vázquez <cvazquez@opennebula.systems>
2019-05-08 16:54:37 +0200

    Update 00-conf_branch_protect.sh

--- .travis/tests/00-conf_branch_protect.sh
+++ .travis/tests/00-conf_branch_protect.sh
@@ -1,16 +1,32 @@
 #!/bin/bash -xv
 
+# **previous** refers to the code base in the main repository
+# **current**  refers to the code base of the PR (or the new commit)
+
 if [[ $TRAVIS_BRANCH =~ (^one-) ]]; then
     export PREVIOUS_ONE=~/previous.one
     export CURRENT_ONE=../../
+    export PREVIOUS_ONE_INSTALL=~/previous.install
+    export CURRENT_ONE_INSTALL=~/current.install    
+    
+    # Checkout previous code base
     git clone https://github.com/tinova/one $PREVIOUS_ONE
     (cd $PREVIOUS_ONE ; git checkout $TRAVIS_BRANCH)
     # In a PR we compare with the head of $TRAVIS_BRANCH
     # Otherwise, we want to check with the previous commit
     if [[ $TRAVIS_PULL_REQUEST == false ]]; then
-        (cd $PREVIOUS_ONE ; git checkout $HEAD~1)
+        (cd $PREVIOUS_ONE ; git checkout HEAD~1)
     fi
-    diff $PREVIOUS_ONE/share/etc/oned.conf $CURRENT_ONE/share/etc/oned.conf
+    
+    # Install previous and current code base
+    $PREVIOUS_ONE/install.sh -d $PREVIOUS_ONE_INSTALL
+    $CURRENT_ONE/install.sh -d $CURRENT_ONE_INSTALL    
+    
+    echo "Testing conf changes:"
+    diff -r $PREVIOUS_ONE_INSTALL/etc $CURRENT_ONE_INSTALL/etc
+    echo $?
+    diff -r $PREVIOUS_ONE_INSTALL/var/remotes/etc $CURRENT_ONE_INSTALL/var/remotes/etc
+    echo $?
 fi
 
 exit -1
